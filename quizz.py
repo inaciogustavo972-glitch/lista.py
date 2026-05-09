c6ac6cc8c46ff222b28ec39fec02c5814ad9ccb8
Gustavo <inaciogustavo972@gmail.com>
2026-05-09 18:23:01 +0000

    v0

--- quizz.py
+++ quizz.py
@@ -1,1 +1,38 @@
+print("---Quizz de Historia---")
+print("1- Qual país deu a Estátua da Liberdade para os Estados Unidos?")
+print("A) França")
+print("B) Inglaterra")
+print("C) Alemanha")
 
+resp1 = input("Sua resposta: ").upper()
+
+if resp1 == "A":
+    print("Você acertou!✅")
+else:
+    print("Você errou!❌")
+    
+print("2- Qual era o nome dos guerreiros do Japão antigo?")
+print("A) Gladiadores")
+print("B) Vikings")
+print("C) Samurais")
+
+resp2 = input("Sua resposta: ").upper()
+
+if resp2 == "C":
+    print("Você acertou!✅")
+else:
+    print("Você errou!❌")
+
+print("3- Quem foi o primeiro homem a pisar na Lua?")
+print("A) Yuri Gagarin")
+print("B) Neil Armstrong")
+print("C) Buzz Aldrin")
+
+resp3 = input("Sua resposta: ").upper()
+
+if resp3 == "B":
+    print("Você acertou!✅")
+else:
+    print("Você errou!❌")       
+    
+#--Gustavo inacio de Oliveira
