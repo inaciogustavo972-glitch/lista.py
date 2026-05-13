e935a77f85a5848682e2734b950e96f9b8b85308
Gustavo <inaciogustavo972@gmail.com>
2026-05-13 14:21:53 +0000

    v1

--- teste.py
+++ teste.py
@@ -1,1 +1,21 @@
-
+#Criando um teste com Pontuação
+print("Teste em Conhecimentos Gerais!")
+pontos = 0
+#--Questão 1--
+print("Qual e a formula da agua ?")
+print(" a- H20 \n b- CO2 \n c- Al ")
+resposta1 = input("Digite a resposta: ").lower()
+if resposta1 == "a":
+    print("|Resposta certa!")
+    pontos = pontos +1
+else:
+    print("Resposta errada!")
+#--Questão 2--
+print("O sol e...??")
+print(" a- Satelite \n b- Estrela \n c- Asteroide ")
+resposta1 = input("Digite a resposta: ").lower()
+if resposta1 == "b":
+    print("|Resposta certa!")
+    pontos = pontos +1
+else:
+    print("Resposta errada!")
