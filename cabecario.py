a1054133b59548a41f3a40ea83d68b680f3324c7
Gustavo <inaciogustavo972@gmail.com>
2026-05-27 13:17:50 +0000

    v2

---
+++ cabecario.py
@@ -0,0 +1,10 @@
+#-- Definindo e chamando função
+def exibir_cabecaalho():
+    print('=' * 30)
+    print("SISTEMA DE GESÂO ESCOLAR")
+    print('Versão 1.0 - Desenvolvido no Codespace')
+    print("=" * 30)
+
+### chamando a função
+exibir_cabecaalho()
+print("Menu principal:")
